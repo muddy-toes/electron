526732e8f1e09c45bb85fac93d900bdd288a14ab
Muddytoes <scottyg28@gmail.com>
2025-03-20 09:59:57 -0400

    only use lowercase for session token
    A prolific driver has an issue pasting the urls
    into discord where it downcases everything, so let's just make
    tokens lowercase.

--- public/js/home.js
+++ public/js/home.js
@@ -1,7 +1,7 @@
 // generates a 10 character long random session ID
 function generateSessionId() {
     let text = "";
-    const possible = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz023456789";
+    const possible = "abcdefghijkmnopqrstuvwxyz023456789";
 
     for (let i = 0; i < 10; i++) {
         text += possible.charAt(Math.floor(Math.random() * possible.length));
